a480b948a1add9aa4a5ccfa36f4260ea7e482ee0
duguBoss <shuozhangsir@gmail.com>
2026-02-25 22:19:25 +0800

    Update fetch_and_ai_process.py

--- fetch_and_ai_process.py
+++ fetch_and_ai_process.py
@@ -58,7 +58,7 @@ def ai_process_gemini(content_text, title_en):
     """
 
     # Gemini API 结构
-    api_url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    api_url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key={GEMINI_API_KEY}"
     
     payload = {
         "contents": [{
